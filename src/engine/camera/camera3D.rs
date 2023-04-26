1b6cfbdda8182e5c31c9d06003279e5e9fbb9741
jaime <JaimeGAlonso@outlook.com>
2023-04-26 20:21:20 +0000

    marginal advances towards ascii rendering

--- src/engine/camera/camera3D.rs
+++ src/engine/camera/camera3D.rs
@@ -8,34 +8,38 @@ struct Camera3D {
     pub dir: Vec3D,
     dist: f32,
 
-    render_size: (u8,u8),
-    raycast_origins: Box<Vec3D>,
+    render_size: (usize,usize),
+    ray_origins: Vec<Vec3D>,
 }
 
 impl Camera3D {
-    pub fn new(&mut self ,pos: Pos3D, dir: Vec3D, render_size: (u8,u8)) {
-        self.pos = pos;
-        self.dir = dir.normalize();
-        self.render_size = render_size;
-        self.dist= (H_FOV as f32).to_radians().tan() * self.render_size.0 as f32/2f32;
+    pub fn new(pos: Pos3D, dir: Vec3D, render_size: (usize,usize)) -> Camera3D {
+        let dist= (H_FOV as f32).to_radians().tan() * render_size.0 as f32/2.;
+        let ray_origins = Vec::<Vec3D>::with_capacity(render_size.0 as usize * render_size.1 as usize); //alocate
 
-        self.local_raycast_coordinates = Box::new(vec![Vec3D::new(),render_size.0 * render_size.1].into_boxed_slice()); //alocate
+        Camera3D { pos, dir: dir.normalize(), dist, render_size, ray_origins}
     }
 
-    fn render(&self, &mut self.local_raycast_coordinates) {
+    fn render(&mut self, render_size: (usize,usize)) {
         const V: Vec3D = Vec3D::y(); 
         // being V a the canonical vector in y direction
         // being dir the vector that points towards the center of the rendering plane
         // if v and dir form basis, then ortho_v is the orthonormalized version of that basis  
-        let ortho_v =  (V - V.dot(&self.dir) / self.dir.norm_squared() * self.dir ).normalize();
-        let ortho_b = ortho_v.cross(&self.dir).normalize(); //R3 cross product is enough to get the third basis
+        let H =  (V - V.dot(&self.dir) / self.dir.norm_squared() * self.dir ).normalize();
+        let W = H.cross(&self.dir).normalize(); //R3 cross product is enough to get the third basis
+        // {W, H, dir} is the most convient basis to generate the rays in each render cycle because W and H are the vector for the X and Y
+        // coordinates realtive to the camera direction, in other words the screen coordinates
+        let basis = (W,H,self.dir);
         
-        let half_width = render_size.0 / 2;
-        let half_height = render_size.1 / 2;
+        let half_width: isize = render_size.0 as isize / 2;
+        let half_height: isize = render_size.1 as isize/ 2;
 
-        for h in -half_height..half_height {
-            for w in -half_width..half_width{
-                *self.local_raycast_coordinates[self.render_size.0 * h ] = 
+        for h_index in 0..render_size.0 - 1 {
+            for w_index in 0..render_size.1 - 1{
+                let ray_index = render_size.0 * h_index + w_index;
+                self.ray_origins[ray_index][0] = 
+                self.ray_origins[ray_index][1] =
+                self.ray_origins[ray_index][2] =
             }
         }
     }
